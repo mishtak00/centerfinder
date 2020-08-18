948c97a4c79b3e31ae2b618c1b4c8be5a331fb5f
mishtak00 <33734216+mishtak00@users.noreply.github.com>
2020-08-18 02:19:05 -0400

    Added kernel type args

--- cfdriver.py
+++ cfdriver.py
@@ -22,31 +22,55 @@ from centerfinder import CenterFinder
 
 
 def main():
-	parser = ArgumentParser(description="~~~~~~~~~~~~~~~~~ ( * ) Center Finder ( * ) ~~~~~~~~~~~~~~~~~")
+	
+	parser = ArgumentParser(description=
+		'~~~~~~~~~~~~~~~~~ ( * ) Center Finder ( * ) ~~~~~~~~~~~~~~~~~')
+
+	# these read input and parameter files
 	parser.add_argument('file', metavar='GALAXY_FILE', type=str, 
-		help='Name of fits file with the galaxy data.')
-	parser.add_argument('-r', '--kernel_radius', type=float, 
-		help='If this argument is present, the kernel radius will be set to the value entered as argument.')
-	parser.add_argument('-t', '--vote_threshold', type=float,
-		help='If this argument is present, centers with number of votes smaller than given argument will be discarded from .fits output.')
+		help='Name of .fits file with the input data.')
+	parser.add_argument('-p', '--params_file', type=str, default='params.json', 
+		help='Sets custom hyperparameters file.')
+	
+	# these define kernel behavior
+	parser.add_argument('-r', '--kernel_radius', type=float, help='Sets kernel radius.')
+	kernel_types = parser.add_mutually_exclusive_group()
+	kernel_types.add_argument('-e', '--step_kernel', action='store_true', 
+		help='Fits a step function to the kernel at kernel radius.')
+	kernel_types.add_argument('-a', '--wavelet_kernel', action='store_true', 
+		help='Fits a wavelet function to the kernel at kernel radius.')
+	kernel_types.add_argument('-g', '--gaussian_kernel', action='store_true', 
+		help='Fits a gaussian function to the kernel at kernel radius.')
+	kernel_types.add_argument('-u', '--custom_kernel', type=str, 
+		help='Fits given custom array to kernel radially.')
+
+	# these define behavior of density grid
+	parser.add_argument('-t', '--vote_threshold', type=float, 
+		help='Centers with number of votes smaller than given argument '\
+		'will be discarded from .fits output.')
 	parser.add_argument('-w', '--weighted_input', action='store_true',
-		help='If this argument is present, CenterFinder will try to read a fourth column from input data and interpret said values as weights.')
+		help='CenterFinder will try to read a fourth column from input data '\
+		'and interpret said values as weights.')
 	con_or_over = parser.add_mutually_exclusive_group()
 	con_or_over.add_argument('-c', '--density_contrast', action='store_true',
-		help='If this argument is present, the CenterFinder will subtract the background from the galaxy density grid before voting.')
+		help='CenterFinder will subtract the background from the galaxy '\
+		'density grid before voting.')
 	con_or_over.add_argument('-o', '--overdensity', action='store_true',
-		help='If this argument is present, the CenterFinder will subtract average density from the galaxy density grid before voting.')
-	parser.add_argument('-p', '--params_file', type=str, default='params.json', 
-		help='If this argument is present, the cosmological parameters will be loaded from file given as argument.')
+		help='CenterFinder will subtract average density from the galaxy '\
+		'density grid before voting.')
+	
+	# ancillary behaviors
 	parser.add_argument('-s', '--save', action='store_true', 
-		help='If this argument is present, grids and .fits output will be automatically saved to an \'out\' folder.')
+		help='Grids and .fits output will be automatically saved to an \'out\' folder.')
 	parser.add_argument('-v', '--verbose', action='store_true', 
-		help='If this argument is present, the progress of CenterFinder will be printed out to standard output.')
+		help='The progress of CenterFinder will be printed out to standard output.')
 	parser.add_argument('-l', '--plot_slice', nargs='*', type=float,
-		help='If this argument is present, CenterFinder will plot a slice of the centers found with galaxies that voted for them superimposed.')
+		help='CenterFinder will plot a slice of the centers found with '\
+		'galaxies that voted for them superimposed.')
+
 	args = parser.parse_args()
 
-	# deletes the .fits extension
+	# deletes the .fits extension and
 	# allows for other '.'s in the args.file string
 	filename = '.'.join(args.file.split('.')[:-1])
 
